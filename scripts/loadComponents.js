8abbd7372446ff234830878f9fe5865c2b67eb88
henruysun2511 <huysun2511@gmail.com>
2025-05-15 17:53:26 +0700

    sua lai chon do an

--- scripts/loadComponents.js
+++ scripts/loadComponents.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
     $("#movie-list").load("components/movieList.html");
     $("#seat").load("components/seat.html");
     $("#food").load("components/food.html");
-     $("#payment").load("components/payment.html");
+    $("#payment").load("components/payment.html");
     $("#footer").load("components/footer.html");
 });
